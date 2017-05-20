3a89623df6abd7efda89fa48ee35efbf2eceaf8c
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-05-20 18:24:56 -0300

    preparacao de ambiente

--- parser/src/test/java/com/github/kyriosdata/aql/AqlTest.java
+++ parser/src/test/java/com/github/kyriosdata/aql/AqlTest.java
@@ -9,8 +9,34 @@
 
 package com.github.kyriosdata.aql;
 
+import org.antlr.v4.runtime.CodePointBuffer;
+import org.antlr.v4.runtime.CodePointCharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.nio.CharBuffer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AqlTest {
 
-    public void aprendendo() {
+    @Test
+    public void aprendendo() throws FileNotFoundException {
+        CodePointCharStream stream = wrapStringEmStream("hello atencao");
+
+        AqlLexer lexer = new AqlLexer(stream);
+        CommonTokenStream cts = new CommonTokenStream(lexer);
+        AqlParser parser = new AqlParser(cts);
+        ParseTree pt = parser.r();
+
+        assertEquals("(r hello atencao)", pt.toStringTree(parser));
+    }
+
+    private CodePointCharStream wrapStringEmStream(String string) {
+        CharBuffer cb = CharBuffer.wrap(string.toCharArray());
+        CodePointBuffer cpb = CodePointBuffer.withChars(cb);
+        return CodePointCharStream.fromBuffer(cpb);
     }
 }
